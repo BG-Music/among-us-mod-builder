30f17bc4114f546f80baefee0d6cd7f67760dfa0
Bossgamerteam <78909975+Bossgamerteam@users.noreply.github.com>
2021-07-13 21:04:53 +0100

    Add files via upload

---
+++ Among-Us-Mod-Builder/Among-Us-Mod-Builder/Build/normal.cmd
@@ -0,0 +1,5 @@
+@echo off
+echo Your mod is building please wait.
+title Among Us Mod Builder
+dotnet build
+pause
---
+++ Among-Us-Mod-Builder/Among-Us-Mod-Builder/ModBulder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ModManager4.Class
+{
+    public class ModBulder
+    {
+        internal object logs;
+        internal string tempPath;
+        internal string token;
+
+        public Version version { get; internal set; }
+        public object utils { get; internal set; }
+
+        internal void log(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void FileDelete(string installerPath)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
